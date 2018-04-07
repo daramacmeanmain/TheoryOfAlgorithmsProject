01b2d51085b7980f071f81176ff4605d45f20974
daramacmeanmain <dara.macmeanmain@outlook.ie>
2018-04-07 23:53:20 +0100

    removed unused code

--- problem2.rkt
+++ problem2.rkt
@@ -1,22 +1,5 @@
 #lang racket
 
-;(define (collatz-list n) 
- ;   (if (= n 3) 
-  ;  (display n)
-   ; (display "n is not 2")))
-;
-;(collatz-list 2)
-
-
-
-;(define (sum l)
-  ;(if (null? l)
-  ;    0
- ;     (+ (car l) (sum (cdr l)))))
-;
-;(sum (list 1 2 3 4 5))
-
-
 (define (collatz-list n)
     (cons n
     (if (= n 1)
